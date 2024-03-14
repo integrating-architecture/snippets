acec71ef39c2f3a978c408af5f540aedf92bdb68
wda <andreas.weidinger@integrating-architecture.de>
2024-03-14 21:49:16 +0100

    update

--- org.isa.snip.js_modules/util/common-classes_mod.js
+++ org.isa.snip.js_modules/util/common-classes_mod.js
@@ -4,6 +4,10 @@
  *  A trivial chain element for building a doubly linked chain.
  */
 export class ChainElem {
+    name = "";
+    next = null;
+    previous = null;
+    
     constructor(pName, pElementsMap = null) {
         this.name = pName;
         pElementsMap ? pElementsMap.set(this.name, this) : null;
